a05ccfd7a9680fbe47f54d223fcb0812142bc7bf
ANGELO FLORES <angeloflores88@gmail.com>
2017-01-28 22:55:20 -0800

    Add files via upload

---
+++ Assignments2/Makefile
@@ -0,0 +1,5 @@
+CC=gcc
+LD=gcc
+CFLAGS=-ggdb -std=c99 -Wall
+LIBS= -lm
+
---
+++ Assignments2/arraysStrings.h
@@ -0,0 +1,9 @@
+//other includes are necessary
+#include "vectors.h"
+//fill in the parameters and return type
+joinrev();
+joinrev_generic();
+readTextAddBinary();
+readBinaryNormText();
+readNormTextWriteNormBinary();
+wc();

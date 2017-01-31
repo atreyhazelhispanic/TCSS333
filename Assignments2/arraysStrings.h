e956bcc068956fc8f9c7b6470b930d1103d7119e
ANGELO FLORES <angeloflores88@gmail.com>
2017-01-31 15:33:52 -0800

    Add files via upload

--- Assignments2/arraysStrings.h
+++ Assignments2/arraysStrings.h
@@ -1,9 +1,11 @@
-//other includes are necessary
+#ifndef Array_Strings
+#define Array_Strings
 #include "vectors.h"
 //fill in the parameters and return type
-char* joinrev(char alpha[], char** beta);
+char* joinrev(char alpha[], char* beta);
 joinrev_generic();
 readTextAddBinary();
 readBinaryNormText();
 readNormTextWriteNormBinary();
 wc();
+#endif //Array_Strings

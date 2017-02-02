bf21fb9cf4bc3d17a57a3ccef3ad2ca609bfa72a
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-02 15:01:52 -0800

    Add files via upload

--- Assignments2/arraysStrings.c
+++ Assignments2/arraysStrings.c
@@ -45,7 +45,7 @@ void* joinrev_generic(void* alpha, void* beta, size_t size, size_t nmembA, size_
 	void* result = malloc((nmembA + nmembB) * size);
 	void* beta2;
 	memcpy(beta2, beta, nmembB);
-	char* = &beta2;
+	char* traverse = (char*) &beta2;
 
 
 

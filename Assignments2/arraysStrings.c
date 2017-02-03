1e9a26dca99bfd034e75725d44b3bec8af21a7e9
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-02 19:12:17 -0800

    Add files via upload

--- Assignments2/arraysStrings.c
+++ Assignments2/arraysStrings.c
@@ -49,21 +49,15 @@ void* joinrev_generic(void* alpha, void* beta, size_t size, size_t nmembA, size_
 	int left = 0;
 	int right = nmembB - 1;
 
-	while(traverse){
-		printf("%s\n", traverse);
-	}
-
-
-	/*
 	while(left < right){
-		char temporary = traverse[left];
+		char temporary <- traverse[left];
 		printf("%s\n",temporary);
 		traverse[left] = traverse[right];
 		traverse[right] = temporary;
 		left++;
 		right--;
 	}
-	
+	/*
 	void* alpha2 = malloc(nmembA * size);
 	char* traverse2 = (char*) alpha2;
 	char* total;

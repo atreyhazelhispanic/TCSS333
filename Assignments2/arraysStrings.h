fbce5f36f07f564a2a7a99723eb8a8df1eb00800
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-02 20:28:59 -0800

    Add files via upload

--- Assignments2/arraysStrings.h
+++ Assignments2/arraysStrings.h
@@ -4,7 +4,7 @@
 //fill in the parameters and return type
 char* joinrev(char alpha[], char* beta);
 void* joinrev_generic(void* alpha, void* beta, size_t size, size_t nmembA, size_t nmembB);
-int readTextAddBinary(char* fileIn, char* fileOut);
+//int readTextAddBinary(char* fileIn, char* fileOut);
 /*readNormTextWriteNormBinary();
 wc();*/
 #endif //Array_Strings

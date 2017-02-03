b6832e80d9ec7468d520c51e24a4be68ff9bac2a
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-02 23:26:51 -0800

    Add files via upload

--- Assignments2/arraysStrings.c
+++ Assignments2/arraysStrings.c
@@ -83,7 +83,7 @@ int readTextAddBinary(char* fileIn, char* fileOut){
         c = fgetc(theRead);
         if(c == '\n');{
         	vector theSum = vector_add(vector_init(first, second, third), vector_init(fourth, fifth, sixth));
-        	fwrite(theSum.x , 4 , sizeof(theSum.x) , theWrite);
+        	fwrite((const void*) theSum.x , 4 , sizeof(theSum.x) , theWrite);
         	fscanf(theRead, "%f %f %f %f %f %f", &first, &second, &third, &fourth, &fifth, &sixth);
         }
         

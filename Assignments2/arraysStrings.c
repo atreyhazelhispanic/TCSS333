74380400592dc6552d1dfd3f83d94257304c16f8
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-04 09:12:52 -0800

    Add files via upload

--- Assignments2/arraysStrings.c
+++ Assignments2/arraysStrings.c
@@ -98,12 +98,10 @@ int readTextAddBinary(char* fileIn, char* fileOut){
 int readBinaryNormText(char* fileIn, char* fileOut){
 	FILE *theRead = fopen(fileIn, "rb");
 	FILE *theWrite = fopen(fileOut, "w+");
-	float first, second, third, fourth, fifth, sixth;
-	fscanf(theRead, "%f %f %f %f %f %f", &first, &second, &third, &fourth, &fifth, &sixth);
 
 	while(!feof(theRead)){
-		printf("%f %f %f %f %f %f\n", &first, &second, &third, &fourth, &fifth, &sixth);
-		fscanf(theRead, "%f %f %f %f %f %f", &first, &second, &third, &fourth, &fifth, &sixth);
+		fread(test, sizeof(theRead), 1, theRead);
+		printf("%s", test);
 	}
 
 	return 0;

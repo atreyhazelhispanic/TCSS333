1aef0f16a5f7d2247eb25e1c5441050f413ec7ea
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-04 10:35:15 -0800

    Add files via upload

--- Assignments2/arraysStrings.c
+++ Assignments2/arraysStrings.c
@@ -96,22 +96,15 @@ int readTextAddBinary(char* fileIn, char* fileOut){
 //Part 4 - 2 points
 //Write a function readBinaryNormText that takes two strings as input (filenames) and returns 0 if the function completes successfully. The first argument is a binary file of type produced in part 3. The function will read it in,  normalize a, and print out to the text file in argument 2, the components, x,y,z, and length separated by tabs (all as one big line) ;
 int readBinaryNormText(char* fileIn, char* fileOut){
-	FILE *theRead = fopen(fileIn, "rb");
+	FILE *theRead = fopen(fileIn, "rt");
 	FILE *theWrite = fopen(fileOut, "wt");
-	FILE *reader = fopen(fileOut, "rt");
 	char* test = malloc(sizeof(theRead));
 
 	while(!feof(theRead)){
 		fread(test, sizeof(theRead), 3, theRead);
 		fwrite(test, sizeof(theRead), 3, theWrite);
+		printf("%s\n", test);
 	}
-
-	char* tester = malloc(sizeof(reader));
-	while(!feof(reader)){
-		fread(tester, sizeof(reader), 3, reader);
-		printf("%s\n", tester);
-	}
-
 	
 	return 0;
 }

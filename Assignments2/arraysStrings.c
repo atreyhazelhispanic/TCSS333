7a2a5835549cd08697afd035690319e78d27304f
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-04 08:15:40 -0800

    Add files via upload

--- Assignments2/arraysStrings.c
+++ Assignments2/arraysStrings.c
@@ -98,7 +98,11 @@ int readTextAddBinary(char* fileIn, char* fileOut){
 int readBinaryNormText(char* fileIn, char* fileOut){
 	FILE *theRead = fopen(fileIn, "rb");
 	FILE *theWrite = fopen(fileOut, "w+");
+	char* test = malloc(sizeof(theRead));
 
+	while(!feof(theRead)){
+
+	}
 
 	return 0;
 }

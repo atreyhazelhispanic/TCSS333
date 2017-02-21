c5c2224c3ba05c20d28796f4a7dd576b5a001931
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-20 23:17:41 -0800

    Add files via upload

--- Assignment3/Assignment-3-master/pixutils.c
+++ Assignment3/Assignment-3-master/pixutils.c
@@ -41,7 +41,7 @@ pixMap *pixMap_read(char *filename,unsigned char arrayType){
   		//can only allocate for the number of rows - each row will be an array of MAXWIDTH
   		//copy each row of the image into each row
 		for(int i=0; p->image[i] != '\0'; i+=MAXWIDTH){
-			&p->image[i] = malloc(MAXWIDTH*sizeof(char *));
+			p->image[i] = (char) malloc(MAXWIDTH*sizeof(char *));
 		}
 	}	
 	else if (arrayType ==1){

084fc517cb9dc8c2dfda443fbab75ffa04cf775e
ANGELO FLORES <angeloflores88@gmail.com>
2017-02-20 16:33:05 -0800

    Add files via upload

--- Assignment3/Assignment-3-master/pixutils.c
+++ Assignment3/Assignment-3-master/pixutils.c
@@ -18,7 +18,7 @@ void pixMap_destroy (pixMap **p){
  		fprintf(stderr, "None exist \n");
  	for(int i=0; i<MAXWIDTH; i++){
  		for(int j=0; j<3; j++){
- 			free &p[i][j];
+ 			free (p[i])+j;
  		}
  	}
 

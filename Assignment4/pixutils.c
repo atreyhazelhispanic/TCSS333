dc5bf0dc20e6ffb049e06042eb27f0f2fa2f2605
ANGELO FLORES <angeloflores88@gmail.com>
2017-03-06 12:54:09 -0800

    Add files via upload

--- Assignment4/pixutils.c
+++ Assignment4/pixutils.c
@@ -167,7 +167,7 @@ static void convolution(pixMap *p, pixMap *oldPixMap,int i, int j,void *data){
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
 			// memcpy(kernel[i]+j, &((int *)data)[counter], sizeof(int));
-			// normalize += ((int *)data)[counter];
+			normalize += ((int *)data)[counter];
 			counter ++;
 		}// taking the array of 9 integers in data and puttin them into a 3x3 kernel
 	}

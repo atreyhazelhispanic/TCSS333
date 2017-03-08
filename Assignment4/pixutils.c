2a26f7bf2e46139d8212e9d7de3eb3071d4eb5e6
ANGELO FLORES <angeloflores88@gmail.com>
2017-03-08 10:30:52 -0800

    Add files via upload

--- Assignment4/pixutils.c
+++ Assignment4/pixutils.c
@@ -111,7 +111,7 @@ plugin *plugin_parse(char *argv[] ,int *iptr){
 	}	
 	if(!strcmp(argv[i]+2,"convolution")){
 	  	new->function = convolution;
-	  	new->data = malloc(9*sizeof(int));
+	  	new->data = (int *)malloc(9*sizeof(int));
 
 	  	// for(int j=0; j<9; j++){
 	  	// 	((int *) new->data)[j] = atoi(argv[i+1+j]);

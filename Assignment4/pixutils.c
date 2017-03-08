97c644d197d5ea0fef7f82a2156e727a785df4ec
ANGELO FLORES <angeloflores88@gmail.com>
2017-03-08 13:20:55 -0800

    Add files via upload

--- Assignment4/pixutils.c
+++ Assignment4/pixutils.c
@@ -175,10 +175,10 @@ static void convolution(pixMap *p, pixMap *oldPixMap,int i, int j,void *data){
 	for(int kernelY=0; kernelY<n; kernelY++){
 		for(int kernelX=0; kernelX<n; kernelX++){
 			int theKern = kernel[kernelY][kernelX];
-			// int theX = (j-padding+kernelX);  
-  	// 		int theY = (i-padding+kernelY); 
-  			// rgba theP = ((rgba**) p->pixArray_overlay)[theX][theY];
-  			// rgba theOld = ((rgba**) oldPixMap->pixArray_overlay)[theX][theY];
+			int theX = (j-padding+kernelX);  
+  			int theY = (i-padding+kernelY); 
+  			rgba theP = ((rgba**) p->pixArray_overlay)[theX][theY];
+  			rgba theOld = ((rgba**) oldPixMap->pixArray_overlay)[theX][theY];
 
   	// 		//extend for edges
   	// 		if(theX<0) theX=0;
@@ -192,10 +192,6 @@ static void convolution(pixMap *p, pixMap *oldPixMap,int i, int j,void *data){
   	// 		theP.a += (theOld.a*theKern)/normalize;
 		}
 	}
-	// for(int i=0; i<n; i++){
-	// 	free(kernel[i]);
-	// }
-	// free(kernel);
 }
 
 //very simple functions - does not use the data pointer - good place to start 

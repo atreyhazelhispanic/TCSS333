3878fccc57275860c706f097434e44b444488ca1
ANGELO FLORES <angeloflores88@gmail.com>
2017-03-08 11:54:55 -0800

    Add files via upload

--- Assignment4/pixutils.c
+++ Assignment4/pixutils.c
@@ -175,8 +175,8 @@ static void convolution(pixMap *p, pixMap *oldPixMap,int i, int j,void *data){
 	for(int kernelY=0; kernelY<n; kernelY++){
 		for(int kernelX=0; kernelX<n; kernelX++){
 			int theKern = kernel[kernelY][kernelX];
-			int theX = (j-padding+kernelX);  
-  			int theY = (i-padding+kernelY); 
+			// int theX = (j-padding+kernelX);  
+  	// 		int theY = (i-padding+kernelY); 
   			// rgba theP = ((rgba**) p->pixArray_overlay)[theX][theY];
   			// rgba theOld = ((rgba**) oldPixMap->pixArray_overlay)[theX][theY];
 

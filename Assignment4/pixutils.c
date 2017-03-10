070fbf1b5ed233815246cd2229ea9bc71152269a
ANGELO FLORES <angeloflores88@gmail.com>
2017-03-09 20:31:30 -0800

    Add files via upload

--- Assignment4/pixutils.c
+++ Assignment4/pixutils.c
@@ -90,7 +90,7 @@ int pixMap_write_bmp16(pixMap *p,char *filename){
  	for(int i=0; i < p->imageHeight; i++){
  		for(int j=0; j < p->imageWidth; j++){
  			// pixel = p->pixArray_overlay[i][j];
- 			pixel = p->pixArray_overlay[(p->pixArray_overlay).length-1-i][j];
+ 			pixel = p->pixArray_overlay[((rgba **) p->pixArray_overlay).length-1-i][j];
  			r16 = pixel.r;
  			g16 = pixel.g;
  			b16 = pixel.b;
